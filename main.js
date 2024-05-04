22981d98813fb3b6f2fdeb593ad4d5c0e57010c5
shortykid <140464830+shortykid@users.noreply.github.com>
2024-05-03 19:26:05 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects= [];
+status = "";
 video = "";
 function preload(){
     video= createVideo('video.mp4')
@@ -9,6 +11,19 @@ canvas.center();
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status != ""){
+        objectDetector.detect(video,gotResult);
+        for (i=0; i< objects.length; i++){
+document.getElementById("status").innerHTML = "Status : Objects Dtected";
+document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+fill("#FF0000");
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15)
+noFill();
+stroke("#FF0000");
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }
+    }
 }
 function modelLoaded(){
     console.log("Model Loaded!")
@@ -20,4 +35,11 @@ video.volume(0);
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function gotResult(error, results){
+    if (errror){
+        console.log(error);
+    }
+console.log(results)
+objects = results
 }
